1c7e41d17483a8692f6a45d3797ba3d7533c5b20
ARGdolphin67 <109750556+ARGdolphin67@users.noreply.github.com>
2023-04-18 14:00:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +15,10 @@ function setup(){
 
 function draw(){
     background('#969A97');
+    document.getElementById("font_size").innerHTML="Font Size of the Text is = "+difference+"px";
+    fill('#F90093')
+    textSize(difference);
+    text('Annabelle',50,400)
 }
 
 function modelLoaded(){
@@ -20,5 +28,14 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX=" +noseX+"noseY=" +noseY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX - rightWristX);
+
+        console.log("leftWristX="+leftWristX+"rightWristX="+rightWristX+"difference"+difference);
     }
 }
